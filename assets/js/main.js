c81cb74e8c17b58b9aa248bddb420913d85b64c6
Mikaela <mikaelac14@gmail.com>
2025-05-15 12:21:04 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,18 @@ window.onscroll = function() {
 function topFunction() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+VANTA.BIRDS({
+  el: "#lead",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x0b0b0b,
+  color1: 0xffffff,
+  color2: 0xffc0cb,
+  birdSize: 1.5,
+  quantity: 3
+});
