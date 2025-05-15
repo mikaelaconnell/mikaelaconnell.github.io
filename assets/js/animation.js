8ad2e7061e1dab7bec35c3500491bb5efa5379ea
Mikaela <mikaelac14@gmail.com>
2025-05-14 21:04:13 -0400

    Update animation.js

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,36 +1,59 @@
+// assets/js/animation.js
+
 const canvas = document.getElementById('bg-animation');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particles = [];
+let triangles = [];
+
+function Triangle() {
+  this.x = Math.random() * canvas.width;
+  this.y = Math.random() * canvas.height;
+  this.size = Math.random() * 10 + 5;
+  this.speedX = (Math.random() - 0.5) * 0.5;
+  this.speedY = (Math.random() - 0.5) * 0.5;
+  this.angle = Math.random() * Math.PI * 2;
+  this.rotationSpeed = (Math.random() - 0.5) * 0.02;
+
+  this.draw = function () {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.beginPath();
+    ctx.moveTo(0, 0 - this.size);
+    ctx.lineTo(0 - this.size, this.size);
+    ctx.lineTo(this.size, this.size);
+    ctx.closePath();
+    ctx.fillStyle = 'rgba(255,255,255,0.15)';
+    ctx.fill();
+    ctx.restore();
+  };
+
+  this.update = function () {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.angle += this.rotationSpeed;
+
+    if (this.x < -50 || this.x > canvas.width + 50 || this.y < -50 || this.y > canvas.height + 50) {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+    }
+
+    this.draw();
+  };
+}
 
 function init() {
+  triangles = [];
   for (let i = 0; i < 100; i++) {
-    particles.push({
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      size: Math.random() * 2 + 1,
-      speedX: (Math.random() - 0.5) * 2,
-      speedY: (Math.random() - 0.5) * 2
-    });
+    triangles.push(new Triangle());
   }
 }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particles.forEach(p => {
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    p.x += p.speedX;
-    p.y += p.speedY;
-
-    if (p.x < 0 || p.x > canvas.width) p.speedX *= -1;
-    if (p.y < 0 || p.y > canvas.height) p.speedY *= -1;
-  });
+  triangles.forEach(t => t.update());
   requestAnimationFrame(animate);
 }
 
